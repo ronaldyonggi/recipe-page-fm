9e3483692957c6d33a067a245df8a92d4494f045
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-25 16:59:44 +0800

    define main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './styles/reset.css';
+import './styles/global.css';
+import RecipeCard from './components/RecipeCard/RecipeCard';
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+export default function App() {
+  return (
+    <main>
+      <RecipeCard />
+    </main>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<App />);
